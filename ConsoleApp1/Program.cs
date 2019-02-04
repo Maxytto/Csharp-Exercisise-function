381da63d75492280d7babde45c8bcd9e5a5c26f9
maxytto <maksimas.asejenkovas@gmail.com>
2019-02-04 19:57:27 +0200

    Task 1 done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,14 +8,32 @@ namespace ConsoleApp1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Max(5,10,7));
+            
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+        static int Max(int sk1, int sk2, int sk3)
+        {
+            if (sk1 > sk2 && sk1 > sk3)
+            {
+                return sk1;
+            }
+            else if (sk2 > sk3 && sk2 > sk1)
+            {
+                return sk2;
+
+           
+            }
+            else
+            {
+                return sk3;
+            }
+        }
+
     }
 }
